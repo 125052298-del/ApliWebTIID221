9e6c5f21099baae9f6ff2196833933b4c46b4db3
125052298-del <125052298@upq.edu.mx>
2026-09-24 15:37:32 -0600

    Practica de cards
     pma

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!--  <?php
-echo "Hola mundo papu"
+echo "Hola mundo papu";
 ?> -->
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +9,29 @@ echo "Hola mundo papu"
     <title>Document</title>
 <style>
 h1{color: blue;}
+
+.section{
+    background-color: lightgreen;
+    padding: 20px 40px;
+    margin-top: 20px;
+    margin-right: 30px;
+    margin-bottom: 40px;
+    margin-left: 50px;
+    border: 10px solid black;
+}
+
+.section2 {
+    background-color: lightpink;
+    padding: 20px 40px;
+
+
+    width: 80%; 
+    margin-top: 20px;
+    margin-bottom: 40px;
+    margin-left: auto;  
+    margin-right: auto;
+}
+
 </style>
 </head>
 <body>
@@ -18,7 +41,16 @@ h1{color: blue;}
     <h4>Hola papu</h4>
     <h5>Hola papu</h5>
     <h6>Hola papu</h6>
+
+    <div class="section">
+                <h2>Hola papu</h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+</div>
+
+<div class="section2">
+    <h2>Título del párrafo</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
 </body>
 </html>
-
-
